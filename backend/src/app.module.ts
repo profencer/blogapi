a27f3bd6d841ad77c1dfa94476b0559a6063c75a
CodeForge <codeforge@example.com>
2025-07-05 05:29:46 +0300

    Add backend/src/app.module.ts

---
+++ backend/src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module, ValidationPipe } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseConfig } from './config/database.config';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfig,
+    }),
+    ConfigModule,
+    TypeOrmModule,
+    UserModule,
+    PostModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
